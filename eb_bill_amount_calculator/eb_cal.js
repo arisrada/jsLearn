c9e51d70790c406dbb52bbb793120b7a48972091
arisrada <arisradad2003@gmail.com>
2023-10-16 19:09:45 +0530

    eb bill calculator

--- eb_bill_amount_calculator/eb_cal.js
+++ eb_bill_amount_calculator/eb_cal.js
@@ -1,3 +1,18 @@
 function ebCalculator() {
-    
+    let units = Number(document.getElementById('input').value);
+    let amount = 0;
+    if (units <= 50){
+        amount = units * 0.75
+    }
+    else if (units > 50 && units <= 150){
+        amount = 50*0.75 + (units-50)*1
+    }
+    else if (units > 150 && units <= 250){
+        amount = 50*0.75 + 100*1 + (units-150)*1.30
+    }
+    else{
+        amount = 50*0.75 + 100*1 + 100*1.30 + (units-250)*1
+    }
+    let result = document.querySelector("#result");
+    result.innerHTML = "Your EB Bill amount is Rs." + amount
 }
