902e79d36a597cd228704744fb6619b5efbeac34
arisrada <arisradad2003@gmail.com>
2023-10-30 17:59:04 +0530

    created on-off

---
+++ on-off-js/index.js
@@ -0,0 +1,14 @@
+var div = document.createElement("div");
+div.setAttribute("class", "div");
+document.body.prepend("div");
+
+const check = document.createElement("input");
+check.type = "checkbox";
+check.setAttribute("id", "check");
+
+const label = document.createElement("label");
+label.setAttribute("for", "check");
+
+document.div.appendChild("check");
+document.div.appendChild("label");
+
